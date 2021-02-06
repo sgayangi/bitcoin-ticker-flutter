9a097aa17eddc9aa6088910aacb0385203815809
sgayangi <sgayangi@gmail.com>
2021-02-07 02:37:33 +0530

    update

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,8 +6,11 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String currency="USD";
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -42,7 +45,20 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: currency,
+              onChanged: (value){
+                print(value);
+                setState(() {
+                  currency=value;
+                });
+              },
+              items: [
+                DropdownMenuItem(child: Text("USD"),value:"USD"),
+                DropdownMenuItem(child: Text("EUR"),value:"EUR"),
+                DropdownMenuItem(child: Text("LKR"),value:"LKR"),
+              ],
+            ),
           ),
         ],
       ),
